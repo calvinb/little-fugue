85b8043b75a4310ce3ed519020b04a5a6051a796
Calvin Bottoms <calvin.bottoms@gmail.com>
2015-09-20 15:43:50 -0500

    Put parts on separate MIDI channels.

--- main.hs
+++ main.hs
@@ -5,5 +5,7 @@ import Alto
 import Tenor
 
 main :: IO ()
-main = play $ tempo (4/8) $ instrument ChurchOrgan parts where
-    parts = Alto.alto :=: transpose (-12) Tenor.tenor
+main = play $ tempo (4/7) parts where
+    alto' = instrument ChurchOrgan Alto.alto
+    tenor' = instrument ChurchOrgan Tenor.tenor
+    parts = alto' :=: transpose (-12) tenor'
