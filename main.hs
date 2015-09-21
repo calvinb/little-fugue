bf7989c9620c06c8f942d55816b699488891bd8c
Calvin Bottoms <calvin.bottoms@gmail.com>
2015-09-20 20:04:38 -0500

    Removed transposition of tenor voice.

--- main.hs
+++ main.hs
@@ -6,6 +6,6 @@ import Tenor
 
 main :: IO ()
 main = play $ tempo (4/7) parts where
-    alto' = instrument ChurchOrgan Alto.alto
+    alto'  = instrument ChurchOrgan Alto.alto
     tenor' = instrument ChurchOrgan Tenor.tenor
-    parts = alto' :=: transpose (-12) tenor'
+    parts  = alto' :=: tenor'
